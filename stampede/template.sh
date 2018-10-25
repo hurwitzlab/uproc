fd46dc2bf8e75474fae2dfcf5cc7a3d40f1ecc87
Ken Youens-Clark <kyclark@gmail.com>
2018-10-25 14:39:26 -0700

    working on the railroad

--- stampede/template.sh
+++ stampede/template.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-echo "QUERY    \"${QUERY}\""
-echo "SEQ_TYPE \"${SEQ_TYPE}\""
-echo "COUNTS   \"${COUNTS}\""
-echo "STATS    \"${STATS}\""
-echo "LONG     \"${LONG}\""
-echo "NUMERIC  \"${NUMERIC}\""
-echo "OTHRESH  \"${OTHRESH}\""
-echo "PTHRESH  \"${PTHRESH}\""
-echo "SHORT    \"${SHORT}\""
-echo "PREDS    \"${PREDS}\""
-
-sh run.sh ${QUERY} ${SEQ_TYPE} ${COUNTS} ${STATS} ${LONG} ${NUMERIC} ${OTHRESH} ${PTHRESH} ${SHORT} ${PREDS}
+sh run.sh ${QUERY} ${READ_LENGTH} ${OTHRESH} ${PTHRESH}
